1b8a5b751efc6a4fdf142324cad00ee424902983
SergioTales1 <algumemailai3@gmail.com>
2023-08-13 14:40:52 -0300

    JS part fixed

--- codeASMR-Product-Card/js/script.js
+++ codeASMR-Product-Card/js/script.js
@@ -1,21 +1,18 @@
 const red = document.getElementById("red"),
-yellow = document.getElementById ("yellow"),
-blue = document.getElementById("blue"),
-productImg = document.getElementById("image"),
-btnBuy = document.querySelector(".btn-buy");
-
+  yellow = document.getElementById("yellow"),
+  blue = document.getElementById("blue"),
+  productImg = document.getElementById("image"),
+  btnBuy = document.querySelector(".btn-buy");
 
 red.addEventListener("click", () => {
-   productImg.setAttribute("src", "../assets/media/shoes1.jpg");
-   btnBuy.style.backgroundColor = "#ab2932";
+  productImg.setAttribute("src", "./assets/media/shoes1.jpg");
+  btnBuy.style.backgroundColor = "#ab2932";
 });
-
 yellow.addEventListener("click", () => {
-   productImg.setAttribute("src", "../assets/media/shoes2.jpg");
-   btnBuy.style.backgroundColor = "#eabb4f"
+  productImg.setAttribute("src", "./assets/media/shoes2.jpg");
+  btnBuy.style.backgroundColor = "#eabb4f";
 });
-
 blue.addEventListener("click", () => {
-   productImg.setAttribute("src", "../assets/media/shoes3.jpg");
-   btnBuy.style.backgroundColor = "#48c3c1"
+  productImg.setAttribute("src", "./assets/media/shoes3.jpg");
+  btnBuy.style.backgroundColor = "#48c3c1";
 });
