3e63d75e2c7a97705212be3fc1f20adb7a6d3fa3
SergioTales1 <algumemailai3@gmail.com>
2023-11-09 14:26:57 -0300

    maps app finished

--- typescript-course/maps/src/index.ts
+++ typescript-course/maps/src/index.ts
@@ -1,4 +1,16 @@
+/// <reference types="@types/google.maps" />
 import { User } from './User';
+import { Company } from './Company';
 
 const user = new User();
-console.log(user);
+// console.log(user);
+
+const company = new Company();
+// console.log(company);
+
+import { CustomMap } from './CustomMap';
+
+const customMap = new CustomMap('map');
+
+customMap.addMarker(user);
+customMap.addMarker(company);
