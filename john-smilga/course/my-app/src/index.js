25f3690b256acf9d1e32507e47f6b47124e5176d
sergiotales1 <algumemailai3@gmail.com>
2023-12-01 18:14:31 -0300

    section-1

--- john-smilga/course/my-app/src/index.js
+++ john-smilga/course/my-app/src/index.js
@@ -1,10 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-function Greeting() {
-  return <h2>My First Component</h2>
+import './index.css'
+
+const BookList = () => {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  )
+}
+
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  )
+}
+
+const Image = () => (
+  <img
+    src="https://m.media-amazon.com/images/I/71CX11nGhWL._SY466_.jpg"
+    alt="Oath and Honor: A Memoir and a Warning"
+  />
+)
+
+const Title = () => <h2>Oath and Honor: A Memoir and a Warning</h2>
+const Author = () => {
+  return <h4>Liz Cheney</h4>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(<Greeting />)
+root.render(<BookList />)
