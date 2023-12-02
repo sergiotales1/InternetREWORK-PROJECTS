fff8835744cf2039caed53ff9772a30d1c37c8cb
sergiotales1 <algumemailai3@gmail.com>
2023-12-02 18:09:29 -0300

    props

--- john-smilga/course/my-app/src/index.js
+++ john-smilga/course/my-app/src/index.js
@@ -3,37 +3,43 @@ import ReactDOM from 'react-dom/client'
 
 import './index.css'
 
+const firstBook = {
+  author: 'Liz Cheney',
+  title: 'Oath and Honor: A Memoir and a Warning',
+  img: '../images/book-1.jpg',
+}
+
+const secondBook = {
+  author: 'Rebecca Yarros',
+  title: 'Fourth Wing (The Empyrean, 1)',
+  img: 'https://m.media-amazon.com/images/I/51j8bRH5sDL._SY445_SX342_.jpg',
+}
+
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      />
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
     </section>
   )
 }
 
-const Book = () => {
+const Book = ({ author, title, img }) => {
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
     </article>
   )
-}
-
-const Image = () => (
-  <img
-    src="https://m.media-amazon.com/images/I/71CX11nGhWL._SY466_.jpg"
-    alt="Oath and Honor: A Memoir and a Warning"
-  />
-)
-
-const Title = () => <h2>Oath and Honor: A Memoir and a Warning</h2>
-const Author = () => {
-  return <h4>Liz Cheney</h4>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
