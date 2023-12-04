e7c53fe32ba9e8a9ce94ce6abc71ff8312f7d00f
sergiotales1 <algumemailai3@gmail.com>
2023-12-04 11:45:50 -0300

    project-done

---
+++ extras/freeCodeCamp-react-wapi/my-app/src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'd235e4dbddmshf5e714abfcd89dep1987f2jsn583ea811da7c',
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
+  },
+}
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
+
+export const WEATHER_API_KEY = '79fca3e68077158d541387e702e5485c'
