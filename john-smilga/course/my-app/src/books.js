7d58228ccb3fa469b03ca410286b19aa04214be8
sergiotales1 <algumemailai3@gmail.com>
2023-12-05 12:42:41 -0300

    section-1-finished

---
+++ john-smilga/course/my-app/src/books.js
@@ -0,0 +1,25 @@
+import img1 from './images/book-1.jpg'
+import img2 from './images/book-2.jpg'
+import img3 from './images/book-3.jpg'
+
+export const books = [
+  {
+    author: 'Liz Cheney',
+    title: 'Oath and Honor: A Memoir and a Warning',
+    img: img1,
+    id: 1,
+  },
+
+  {
+    author: 'Jordan Moore',
+    title: 'Interesting Facts For Curious Minds',
+    img: img2,
+    id: 2,
+  },
+  {
+    author: 'Suzanne Collins',
+    title: 'The Ballad of Songbirds and Snakes',
+    img: img3,
+    id: 3,
+  },
+]
--- john-smilga/course/my-app/src/index.js
+++ john-smilga/course/my-app/src/index.js
@@ -1,42 +1,20 @@
-import React, { Children } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './index.css'
-
-const books = [
-  {
-    author: 'Liz Cheney',
-    title: 'Oath and Honor: A Memoir and a Warning',
-    img: '../images/book-1.jpg',
-    id: 1,
-  },
-
-  {
-    author: 'Rebecca Yarros',
-    title: 'Fourth Wing (The Empyrean, 1)',
-    img: 'https://m.media-amazon.com/images/I/51j8bRH5sDL._SY445_SX342_.jpg',
-    id: 2,
-  },
-]
+import { books } from './books'
+import Book from './Book'
 
 const BookList = () => {
   return (
-    <section className="booklist">
-      {books.map((el) => {
-        console.log(el, { ...el })
-        return <Book {...el} key={el.id} />
-      })}
-    </section>
-  )
-}
-
-const Book = ({ author, title, img }) => {
-  return (
-    <article className="book">
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <h4>{author}</h4>
-    </article>
+    <>
+      <h1>amazon best sellers</h1>
+      <section className="booklist">
+        {books.map((el, index) => {
+          return <Book {...el} key={el.id} number={index} />
+        })}
+      </section>
+    </>
   )
 }
 
